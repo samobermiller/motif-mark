aeba67869f4a1d270761be81868f7ff0e2f8d823
samobermiller <samobermiller@gmail.com>
2023-02-23 16:33:55 -0800

    still need to fix colors

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -5,11 +5,13 @@ def get_args():
     parser.add_argument("-f", "--file", help="fasta file")
     parser.add_argument("-m", "--motif", help="motifs file")
     return parser.parse_args()
+##OOP
 class motif:
-    def __init__(self, sequence):
+    def __init__(self, sequence,color):
         '''This is how a motif is made'''
         ## Data ##
         self.sequence = sequence
+        self.color=color
     ## Method ##
     def fix_ambiguity(self,sequence):
         "take motif and generate regex expression that can be used to search sequence while accounting for any ambiguity from motif or sequence"
@@ -21,131 +23,127 @@ class motif:
             fixed_sequence+=fix[letter]
         return[fixed_sequence]
         #creates regular expression based on given motif that you can use to search input sequence
-#TEST for class motif
+
+##Test class motif generation
 # practice=motif('catag')
 # amb=practice.fix_ambiguity(practice.sequence)
 # print(amb[0])
 
-##Generate motif regex statements
 args=get_args()
-motifs=[]
+possible_motifs=[]
+i=0
+motif_classes=[]
+color=[[0,0.2,0.8],[0.6,0,0.4],[1,0,0],[0.5,0.2,0.3],[0.3,0.7,0.2]]
 with open(args.motif, "r") as motif_list:
     for line in motif_list:
-        motifs.append(line.strip())
-possible_motifs=[]
-for item in motifs:
-    item_fix=motif(item)
-    possible_motifs+=item_fix.fix_ambiguity(item)
-#print(possible_motifs[1])
+        line=line.strip('\n')
+        item_fix=motif(line,color[i])
+        i+=1
+        possible_motifs+=item_fix.fix_ambiguity(line)
+        motif_classes.append(item_fix)
 #possible_motifs now stores regex expressions for each motif given in args.motif
+
+
+##Open files and start counters
 import re
 match=re.findall(r'[ \w-]+?(?=\.)', args.file)
 output_filename=match[0]
-output=open(f'{output_filename}.fa', "w")
-count_lines=0
-#Open canvas
+
+#open canvas
 import cairo
-##Parse motif and fasta file by header
-headers=0
-#number of headers
-start=10
-#start point, running sum
-total_bases=0
-#total number of nucleotides
-lower_bases=0
-#intron bases
-upper_bases=0
-#exon bases
-motif_base=0
-#bases in motifs
-count_nomotif=0
-count_motif=0
-#number of motifs found
-surface = cairo.SVGSurface(f"{output_filename}_plot.svg", 1000, 100)
+surface = cairo.SVGSurface(f"{output_filename}_plot.svg", 1000, 500)
 context = cairo.Context(surface)
-with open(args.file,"r") as fasta:
-    #PER LINE
-    for line in fasta:
+
+##Make Fasta file input into single line sequence
+sequence=""
+header=""
+with open(f"./{output_filename}_one_line.fa", "w") as output:
+    with open(args.file,"r") as fasta:
+        for line in fasta:
+            line=line.strip('\n')
+            if ">" in line:
+                if header != "":
+                    output.write(header+"\n")
+                    output.write(sequence+"\n")
+                    sequence=""
+                header=line
+            else:
+                sequence+=line
+        output.write(header+"\n")
+        output.write(sequence+"\n")
+motif_dict={}
+#motif_seq: start,end
+##Parse new fasta file
+line_value=10
+with open(f"./{output_filename}_one_line.fa", "r") as new_fasta:
+    for line in new_fasta:
         if line.startswith(">"):
-            headers+=1
-            output.write(line)
+            continue
         else:
             line=line.strip('\n')
+            line_value+=40
+            sequence_length=len(line)
+            new_line=re.split(f'[a-z][A-Z]|[A-Z][a-z]',line)
+            #print(new_line)
+            start=10
+            end=start
+            for segment in new_line:
+                #print(segment)
+                segment_length=len(segment)
+                end=start+segment_length
+                context.set_source_rgb (0, 0, 0)
+                if segment.isupper():
+                    #print(f"exon length {segment_length}")
+                    #print(f"start position {start}")
+                    #print(f"end position {end}")
+                    context.set_line_width(30)
+                    context.move_to(start,line_value)
+                    context.line_to(end,line_value)
+                    context.stroke()
+                if segment.islower():
+                    #print(f"intron length {segment_length}")
+                    #print(f"start position {start}")
+                    #print(f"end position {end}")
+                    context.set_line_width(5)
+                    context.move_to(start,line_value)
+                    context.line_to(end,line_value)
+                    context.stroke()
+                start+=segment_length
             motif_match=()
-            for entry in possible_motifs:
-                motif_match=re.findall(f'{entry}', line)
-                #list of motif matches in the current line based on regex created with class motif
-            #if the number of motifs in the line is not 0 then:
-            if len(motif_match)!=0:
-                #print(motif_match)
-                #PER MOTIF
-                for motif_seq in motif_match:
-                    #for each motif match in the line
-                    count_motif+=1
-                    pre_motif=re.findall(f'.*(?={motif_seq})', line)
-                    pre_motif=pre_motif[0]
-                    #pre_motif is the sequence before the current motif
-                    #PER BASE
-                    for base in pre_motif:
-                    #differentiate exons and introns before motif match
-                        total_bases+=1
-                        start+=1
-                        context.move_to(start,40)
-                        if base.islower():
-                            #print(base)
-                            lower_bases+=1
-                            context.set_line_width(5)
-                            context.move_to(start,40)
-                            context.line_to(start+1,40)
-                            context.stroke()
-                        if base.isupper():
-                            upper_bases+=1
-                            context.set_line_width(30)
-                            context.move_to(start,40)
-                            context.line_to(start+1,40)
-                            context.stroke()
-                    for base in motif_seq:
-                        total_bases+=1
-                        start+=1
-                        motif_base+=1
-                        context.move_to(start,40)
+            for single in motif_classes:
+                possible_motifs+=single.fix_ambiguity(single.sequence)
+                #print(single.color)
+                for entry in possible_motifs:
+                    motif_match=re.findall(f'{entry}', line)
+                    for motif_seq in motif_match:
+                        #for each motif match in the segment
+                        pre_motif=re.findall(f'.*(?={motif_seq})', line)
+                        #motif_color=
+                        #print(motif_seq)
+                        #print(pre_motif[0])
+                        motif_length=len(motif_seq)
+                        motif_start=len(pre_motif[0])
+                        motif_end=motif_start+motif_length
+                        motif_dict[motif_seq]=motif_start,motif_end
+                        #print(motif_start)
+                        #print(motif_length)
+                        #print(f"motif length {motif_length}")
+                        #print(f"start position {motif_start}")
+                        #print(f"end position {motif_end}")
                         context.set_line_width(30)
-                        context.move_to(start,40)
-                        context.set_source_rgba(4, 0, 4, 0.5)
-                        context.line_to(start+1,40)
-                        context.stroke()                   
-            if len(motif_match)==0:
-                count_nomotif+=1
-                context.set_source_rgba (0, 0, 0, 1)
-                #PER BASE
-                for base in line:
-                    total_bases+=1
-                    start+=1
-                    #context.move_to(leftright,updown)
-                    context.move_to(start,40)
-                    if base.islower():
-                        #print(base)
-                        lower_bases+=1
-                        context.set_line_width(5)
-                        context.move_to(start,40)
-                        context.line_to(start+1,40)
+                        color1=single.color[0]
+                        color2=single.color[1]
+                        color3=single.color[2]
+                        print(single.color)
+                        
+                        context.set_source_rgb(color1, color2, color3)
+                        context.move_to(motif_start,line_value)
+                        context.line_to(motif_end,line_value)
                         context.stroke()
-                    if base.isupper():
-                        upper_bases+=1
-                        context.set_line_width(30)
-                        context.move_to(start,40)
-                        context.line_to(start+1,40)
-                        context.stroke()
-        count_lines+=1
+                        #print(start)
+                        #whats the length of the segment?
+                        #is it upper or lower case?
 surface.write_to_png(f"{output_filename}_plot.png")
 surface.finish()
-print(f"# of Headers= {headers}")
-print(f"Total Lines in File= {count_lines}")
-print(f"# of Motifs= {count_motif}")
-print(f"# of Motif Bases= {motif_base}")
-print(f"# of Exon Bases= {upper_bases}")
-print(f"# of Introns Bases= {lower_bases}")
-print(f"# Total Bases= {total_bases}")
-
-
-##Current issue: not all motifs are depicted in figure getting drawn. keep making counts to find where issue is, total bases checks out
+# for single in motif_classes:
+#     print(single.color)
